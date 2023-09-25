410086892d903131fa8de123909965d3b3a2e8aa
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2023-09-25 18:41:49 +0200

    Changed how colors are generated

--- svgGenerations.js
+++ svgGenerations.js
@@ -1,57 +1,47 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-function generateSVG(blockNumber, tokenId) {
-    // Combine blockNumber and tokenId to create a unique seed
-    const uniqueSeed = blockNumber.toString() + tokenId.toString();
+function generateSVG() {
+    // Define an array of colors
+    const colors = ['#0000FF', '#FF0000', '#800080', '#008000', '#FFA500', '#FFFF00', '#FFD700', '#FFFFFF', '#C0C0C0'];
 
-    // Generate a color based on the unique seed
-    const hash = crypto.createHash('sha256').update(uniqueSeed).digest('hex');
-    const dynamicColor = `#${hash.slice(0, 6)}`; // Use the first 6 characters of the hash as the dynamic color
+    // Initialize an empty array to store SVG strings
+    const svgStrings = [];
 
-    // Calculate the brightness of the dynamic color (higher value means brighter color)
-    const brightness = calculateBrightness(dynamicColor);
+    // Loop through the colors and generate an SVG for each
+    colors.forEach((color) => {
+        // Create the SVG string with the specified size and the current color
+        const svgString = `
+            <svg width="550" height="550" xmlns="http://www.w3.org/2000/svg">
+                <!-- Transparent background rectangle -->
+                <rect width="100%" height="100%" fill="none" />
 
-    // Determine the text color based on the brightness
-    const textColor = brightness > 128 ? 'black' : 'white';
+                <!-- Slightly thinner black edge circle -->
+                <circle cx="275" cy="275" r="245" fill="black" />
 
-    // Create the SVG string with the specified size
-    const svgString = `
-        <svg width="550" height="550" xmlns="http://www.w3.org/2000/svg">
-            <!-- Transparent background rectangle -->
-            <rect width="100%" height="100%" fill="none" />
+                <!-- Button with gradient -->
+                <defs>
+                    <linearGradient id="buttonGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                        <stop offset="0%" style="stop-color:${color};stop-opacity:1" />
+                        <stop offset="100%" style="stop-color:rgba(255,255,255,0.5);stop-opacity:1" />
+                    </linearGradient>
+                </defs>
+                <circle cx="275" cy="275" r="200" fill="url(#buttonGradient)" />
 
-            <!-- Slightly thinner black edge circle -->
-            <circle cx="275" cy="275" r="245" fill="black" />
+                <!-- Text element with dynamically determined text color and Inter font -->
+                <text x="50%" y="50%" font-size="20" text-anchor="middle" dominant-baseline="middle" fill="white" font-family="Inter, sans-serif">Zora Mint Button</text>
+            </svg>
+        `;
 
-            <!-- Button with gradient -->
-            <defs>
-                <linearGradient id="buttonGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-                    <stop offset="0%" style="stop-color:${dynamicColor};stop-opacity:1" />
-                    <stop offset="100%" style="stop-color:rgba(255,255,255,0.5);stop-opacity:1" />
-                </linearGradient>
-            </defs>
-            <circle cx="275" cy="275" r="200" fill="url(#buttonGradient)" />
+        // Push the SVG string to the array
+        svgStrings.push(svgString);
 
-            <!-- Text elements with dynamically determined text color and Inter font -->
-            <text x="50%" y="45%" font-size="20" text-anchor="middle" dominant-baseline="middle" fill="${textColor}" font-family="Inter, sans-serif">Zora Mint Button</text>
-            <text x="50%" y="55%" font-size="14" text-anchor="middle" fill="${textColor}" font-family="Inter, sans-serif">Block: ${blockNumber}</text>
-            <text x="50%" y="59%" font-size="14" text-anchor="middle" fill="${textColor}" font-family="Inter, sans-serif">Token ID: ${tokenId}</text>
-        </svg>
-    `;
+        // Save the SVG to a file named after the color
+        const colorName = color.slice(1); // Remove the '#' character
+        fs.writeFileSync(`${colorName}.svg`, svgString);
+    });
 
-    // Save the SVG to a file
-    fs.writeFileSync('output.svg', svgString);
-
-    return svgString;
-}
-
-// Function to calculate brightness from a hex color code
-function calculateBrightness(hexColor) {
-    const r = parseInt(hexColor.slice(1, 3), 16);
-    const g = parseInt(hexColor.slice(3, 5), 16);
-    const b = parseInt(hexColor.slice(5, 7), 16);
-    return (r * 299 + g * 587 + b * 114) / 1000;
+    return svgStrings;
 }
 
 module.exports = generateSVG;
