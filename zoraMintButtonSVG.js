10d19d30969c196ec465fd27b7fd2de39895c4a1
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2023-09-25 10:24:18 +0200

    Changed function parameters for generating colors

--- zoraMintButtonSVG.js
+++ zoraMintButtonSVG.js
@@ -1,4 +1,4 @@
 const generateSVG = require('./svgGenerations');
 
-const svg = generateSVG("0xa4B0bfcD985fE1AdBFF2ade8C38E0E6EF335e985", 18198145, 1);
+const svg = generateSVG(1819, 2);
 console.log(svg);
